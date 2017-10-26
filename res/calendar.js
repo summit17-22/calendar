e96cacbed0ec8b18a4d5bab1ce71f13ddf507b88
summit17-22 <33107526+summit17-22@users.noreply.github.com>
2017-10-25 20:57:33 -0700

    Add files via upload

--- res/calendar.js
+++ res/calendar.js
@@ -4,10 +4,13 @@ function init() {
     Tabletop.init({
         key: key,
         callback: function(data, tabletop) {
-            console.log(data[1])
+            console.log(data)
         },
         simpleSheet: true
     })
 }
 
 window.addEventListener('DOMContentLoaded', init)
+
+var subjects = ["Socials", "English", "Math", "Science"]
+var colors = ["#d32f2f", "#2e7d32", "#1565c0", "#ffee58"]
