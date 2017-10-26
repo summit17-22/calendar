cc337936bfd6031c8ae8e341a66871de5d0d7442
summit17-22 <33107526+summit17-22@users.noreply.github.com>
2017-10-25 18:06:25 -0700

    Script reupload

--- src/calendar.js
+++ src/calendar.js
@@ -5,28 +5,6 @@ function init() {
         key: key,
         callback: function(data, tabletop) {
             console.log(data[0])
-            
-            var assignment = data[0]
-            
-            var print = "Assignment <b>";
-            
-            print += assignment.name
-            
-            print += "</b> is from <b>"
-            
-            print += assignment.class
-            
-            print += "</b> class and is due on <b>"
-            
-            print += assignment.duedisplay
-            
-            print += "</b>. It has criteria <b>"
-            
-            print += assignment.criteria
-            
-            print += "</b>"
-           
-            document.write(print)
         },
         simpleSheet: true
     })
